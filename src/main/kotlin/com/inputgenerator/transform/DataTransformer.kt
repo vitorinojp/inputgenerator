bfdb85c1301e5ea477b3f28633ce99c3fb36931c
João Pedro Vitorino <jvitorino@deetc.isel.ipl.pt>
2022-06-13 19:33:54 +0100

    Style fixes

--- src/main/kotlin/com/inputgenerator/transform/DataTransformer.kt
+++ src/main/kotlin/com/inputgenerator/transform/DataTransformer.kt
@@ -3,33 +3,36 @@ package com.inputgenerator.transform
 import com.inputgenerator.entities.BaseEntity
 import com.inputgenerator.entities.DataEntity
 
-interface DataTransformer <in A, B> {
+interface DataTransformer<in A, B> {
     fun apply(a: A): DataEntity<B>?
     fun getDescription(): String?
 }
 
-class PassAllTransformer <A> (): DataTransformer<A, A> {
+class PassAllTransformer<A>() : DataTransformer<A, A> {
     override fun apply(a: A): DataEntity<A>? {
         return BaseEntity(a)
     }
+
     override fun getDescription(): String? {
         return this.javaClass.name
     }
 }
 
-class DummyTransformer: DataTransformer<Any, Unit> {
+class DummyTransformer : DataTransformer<Any, Unit> {
     override fun apply(a: Any): BaseEntity<Unit>? {
         return null
     }
+
     override fun getDescription(): String? {
         return this.javaClass.name
     }
 }
 
-class StringTransformer: DataTransformer<String, String> {
+class StringTransformer : DataTransformer<String, String> {
     override fun apply(a: String): BaseEntity<String>? {
         return BaseEntity(a)
     }
+
     override fun getDescription(): String? {
         return this.javaClass.name
     }
