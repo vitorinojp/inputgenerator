fcc3c662a46a088b0c59eb4f9fc13c79a84256cc
João Pedro Vitorino <jvitorino@deetc.isel.ipl.pt>
2022-06-13 19:32:08 +0100

    Fix sink metrics

--- src/main/kotlin/com/inputgenerator/sinks/DataSink.kt
+++ src/main/kotlin/com/inputgenerator/sinks/DataSink.kt
@@ -5,34 +5,34 @@ import com.inputgenerator.metrics.AtomicLongMetric
 import com.inputgenerator.metrics.IMetricCounter
 import com.inputgenerator.metrics.MetricsRepository
 
-interface DataSink <V> {
+interface DataSink<V> {
     fun write(data: DataEntity<V>): DataEntity<V>
     fun getDescription(): String?
     fun init(configs: Map<String, String> = HashMap())
 }
 
-abstract class BaseDataSink<V> (
+abstract class BaseDataSink<V>(
     sequenceName: String,
     sinkName: String,
     var metricsRepository: MetricsRepository,
     var sinkId: String = "${sequenceName}.${sinkName}"
-): DataSink<V>{
+) : DataSink<V> {
     protected var writeMetric: IMetricCounter? = null
     protected var failsMetric: IMetricCounter? = null
 
     override fun init(configs: Map<String, String>) {
-        metricsRepository.registerMetricIfAbsent("sequence.sink.${sinkId}.writes", AtomicLongMetric())
-        metricsRepository.registerMetricIfAbsent("sequence.sink.${sinkId}.fails", AtomicLongMetric())
+        metricsRepository.registerMetricIfAbsent("sequence.${sinkId}.writes", AtomicLongMetric())
+        metricsRepository.registerMetricIfAbsent("sequence.${sinkId}.fails", AtomicLongMetric())
 
-        this.writeMetric = metricsRepository.getCounter("sequence.sink.${sinkId}.writes")
-        this.failsMetric = metricsRepository.getCounter("sequence.sink.${sinkId}.fails")
+        this.writeMetric = metricsRepository.getCounter("sequence.${sinkId}.writes")
+        this.failsMetric = metricsRepository.getCounter("sequence.${sinkId}.fails")
     }
 }
 
-class DummySink <V> (
+class DummySink<V>(
     sequenceName: String,
     metricsRepository: MetricsRepository
-): BaseDataSink<V>(sequenceName, "dummySink", metricsRepository) {
+) : BaseDataSink<V>(sequenceName, "dummySink", metricsRepository) {
     override fun write(data: DataEntity<V>): DataEntity<V> {
         return data
     }
